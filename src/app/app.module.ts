1487a759b26cd8fcffe756113a5193851471c654
shobitkhatri05 <shobitkhatri62@gmail.com>
2019-07-31 13:28:40 +0530

    Form2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MiddleComponent } from './middle/middle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiddleComponent
   ],
   imports: [
     BrowserModule,
